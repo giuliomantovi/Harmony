042855e208bc7014d7fd33af0baf92e6daf4367d
giuliomantovi <giulio.mantovi@libero.it>
2023-05-08 19:19:16 +0200

    graphic changes

---
+++ src/main/java/com/gmantovi/harmony/homeController.java
@@ -0,0 +1,30 @@
+package com.gmantovi.harmony;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
+public class homeController {
+    @FXML private AnchorPane homeAnchorPane;
+    @FXML private Button topArtists;
+    @FXML private Button topSongs;
+
+    @FXML
+    public void initialize() throws IOException {
+
+
+    }
+
+    @FXML
+    public void showTopArtists() throws IOException {
+
+    }
+
+    @FXML
+    public void showTopSongs() throws IOException {
+
+    }
+
+}
