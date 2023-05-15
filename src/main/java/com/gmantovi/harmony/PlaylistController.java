0016a53cc75c8782b684d37638f44ed30d75e643
giuliomantovi <giulio.mantovi@libero.it>
2023-05-15 21:11:41 +0200

    ...

--- src/main/java/com/gmantovi/harmony/PlaylistController.java
+++ src/main/java/com/gmantovi/harmony/PlaylistController.java
@@ -46,7 +46,7 @@ public class PlaylistController {
         Connection connection = null;
         Statement statement = null;
         try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName("com.mysql.jdbc.Driver");
             connection = DriverManager.getConnection("jdbc:mysql://localhost/harmony?user=root&password=giulio");
             statement = connection.createStatement();
             ResultSet rs = statement.executeQuery("SELECT * FROM playlist");
